c334b63b792e376e621226f4dd0b8759f6bfe7ea
Lagani21 <lpatel@ncsu.edu>
2025-05-04 16:27:50 -0400

    tested dataset for task A

---
+++ demo/task_a_check.py
@@ -0,0 +1,15 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.dataset_a import TaskADataset
+from torch.utils.data import DataLoader
+
+
+dataset = TaskADataset("data/task_a_samples.json")
+loader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+for batch in loader:
+    print(batch['input_ids'].shape, batch['label'])  # Sanity check
+    break
+ 
